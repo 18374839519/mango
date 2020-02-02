a6174f3a3176b9437968d4f7fa747316972f50d0
18374839519 <dengdongxu1992@163.com>
2020-02-02 21:02:24 +0800

    删除master代码

--- mango-common/src/main/java/com/louis/mango/common/utils/datas/GlobalContents.java
+++
@@ -1,7 +0,0 @@
-package com.louis.mango.common.utils.datas;
-
-public class GlobalContents {
-
-    // 用户表导出表头
-    public final static String[] userTitleStr = {"编号", "用户名", "昵称", "邮箱", "手机号", "状态", "机构编号", "创建人", "创建时间", "更新人", "更新时间", "是否删除"};
-}
